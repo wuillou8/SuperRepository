a92c0c675cad7035289b002e7bec4ae874c10860
jair <jair@jair-s-MacBook-2.local>
2013-06-07 17:56:38 +0200

    commit

--- Python/BayesianNaive/PLOT/scriptplot.sh
+++ Python/BayesianNaive/PLOT/scriptplot.sh
@@ -2,6 +2,7 @@ rm -f tmp.tex tmp.ps  tmp.dvi $1.ps  $1.png
 gnuplot $1
 latex tmp.tex
 dvips -o tmp.ps tmp.dvi
-cp  tmp.ps $1.ps
-cp tmp.ps $1.ps
+mv tmp.dvi $1.dvi
+mv tmp.ps $1.ps
 ps2pdf $1.ps
+rm tmp*
