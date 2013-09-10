e558669f50aed0d03316c9d1c13b23daad231767
wuillou8 <wuilloud@gmail.com>
2013-09-10 22:11:28 +0200

    Improved Bayesian

--- MLearning/Python/MLExercises/NaiveBayesian/NaiveBayesian.py
+++ MLearning/Python/MLExercises/NaiveBayesian/NaiveBayesian.py
@@ -10,47 +10,74 @@
 #-------------------------------------------------------------------------------
 import io, os, collections, csv, string, re, sys
 from numpy import *
-
+import panda as pd
 import matplotlib
 matplotlib.rcParams['legend.fancybox'] = True
 import matplotlib.pyplot as plt
 import pylab
 
+
 class NaiveBayesian:
     ############################################################################
     #       Class: Naive Bayesian Analysis                                     #
     ############################################################################
-    def __init__(self, dirLearn):
-        self.dirLearn = os.getcwd()+"\\"+dirLearn
-        self.dirFiles = [f for f in os.listdir(self.dirLearn)]
-        self.Dict = self.MakeDict()
-        self.Train = self.GenerTriplet()
-        for i in [1,2,3]:
-            print self.Train[i]
+    def __init__(self, dirLearnspam, dirLearnnospam):
+        self.dirLearnspam = os.getcwd()+"//DATA//ex6DataEmails//"+dirLearnspam
+	self.dirLearnnospam = os.getcwd()+"//DATA//ex6DataEmails//"+dirLearnnospam
+        self.dirFilesspam = [f for f in os.listdir(self.dirLearnspam)]
+	self.dirFilesnospam = [f for f in os.listdir(self.dirLearnnospam)]
+        self.DictSpam = self.MakeDict("spam")
+	self.DictnoSpam = self.MakeDict("non-spam")
 
-    def MakeDict(self):
+    def MakeDict(self,spamornot):
         MakeDict = {}
-        for tfile in self.dirFiles:
-            filename= self.dirLearn+"\\"+tfile
-            for word in sorted( filter( lambda x: len(x) > 2, [tfile for tfile in loadtxt(filename,dtype='str') ] ) ):
-                MakeDict[word] = MakeDict.get(word, 0) + 1
+	
+	if (spamornot == "spam"):
+		files = self.dirFilesspam
+		dirLearn = self.dirLearnspam
+	elif (spamornot == "non-spam"):
+		files = self.dirFilesnospam
+		dirLearn = self.dirLearnnospam
+	
+	for tfile in files:
+        	filename= dirLearn+"//"+tfile
+            	for word in sorted( filter( lambda x: len(x) > 2, [tfile for tfile in loadtxt(filename,dtype='str') ] ) ):
+                	MakeDict[word] = MakeDict.get(word, 0) + 1
         # Sorted in the alphab. order
         return MakeDict
 
-    def GenerTriplet(self):
-        # (docID, wordID, count)
-        GenerTriplet = {}
-        for n_file, tfile in enumerate(self.dirFiles):
-            filename= self.dirLearn+"\\"+tfile
-            for word in sorted( filter( lambda x: len(x) > 2, [tfile for tfile in loadtxt(filename,dtype='str') ] ) ):
-                GenerTriplet[word] = [ GenerTriplet.get(word, 0) , 1 ]
-        return GenerTriplet
+    def buildProb(self, testDir):
+	# fction analysing the probas spam/non-spam 
+	# based on the Dictionaries computed above
+	buildProb = []
+	dirTested = os.getcwd()+"//DATA//ex6DataEmails//"+testDir
 
+	fff = open(testDir+".txt", 'w')
+	fff.write('hi there\n') 
+	for tfile in [f for f in os.listdir(dirTested)]:
+            	fileName= dirTested+"//"+tfile
+		PSpam = 0. 
+		PnoSpam = 0.
+		for word in sorted( filter( lambda x: len(x) > 0, loadtxt(fileName,dtype='str') ) ):
+			PSpam += self.DictSpam.get(word, 0) + 1
+    			PnoSpam += self.DictnoSpam.get(word, 0) + 1
 
+		if( log(PSpam) >= log(PnoSpam) ):
+			fff.write( tfile+" spam\n" )
+		else:
+			fff.write( tfile+" non-spam\n" )
+	pass
+
+#####################################################################
 def main():
     pass
 
 if __name__ == '__main__':
     main()
+#####################################################################
 
-l_naiveB = NaiveBayesian("spam-train") # 5 for the poly_order
+# Init over nonspam/spam dirs in DATA/ex6DataEmails/
+l_naiveB = NaiveBayesian("spam-train","nonspam-train") 
+# Analysis run over the nonspam/spam dirs in DATA/ex6DataEmails/
+l_naiveB.buildProb("nonspam-test")
+l_naiveB.buildProb("spam-test")
