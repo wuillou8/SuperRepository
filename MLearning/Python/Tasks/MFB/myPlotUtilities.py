2dbc05552168af17e39a7a018b95d62fe15ff156
wuillou8 <wuilloud@gmail.com>
2013-10-26 13:49:31 +0200

    data analysis with pandas

---
+++ MLearning/Python/Tasks/MFB/myPlotUtilities.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        myPlotUtilities
+# Purpose:
+#
+# Author:      wuillou8
+#
+# Created:     25/10/2013
+# Copyright:   (c) wuiljai 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import pylab as P
+import matplotlib
+matplotlib.rcParams['legend.fancybox'] = True
+import matplotlib.pyplot as plt
+import math
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+def plotGraph(X,Y):
+      fig = plt.figure()
+      ### Plotting arrangements ###
+      return fig
