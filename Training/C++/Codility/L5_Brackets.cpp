55008bcd6d97c58d69248c06cedf4b4b638ef04e
wuillou8 <wuilloud@gmail.com>
2014-03-28 22:07:27 +0100

    codility stuff added

--- Training/C++/Codility/L5_Brackets.cpp
+++ Training/C++/Codility/L5_Brackets.cpp
@@ -1,16 +1,38 @@
+
 // you can also use includes, for example:
-// #include <algorithm>
+#include <algorithm>
 int solution(const string &S) {
     // write your code in C++98
-    string copy(S);
+    // 50% Solution
+    string cp(S);
+    size_t pos = 0, marker = 0; 
+    string tmp;
+
+    int loop=0;
+    do {
+        pos = 0;
+        marker = cp.size();
+        while ( (pos+1 < cp.size()) & (cp.size() > 0) ) {
+            tmp = cp.substr (pos, 2);
+            if ( tmp == "()" || tmp == "{}" || tmp == "[]") {
+                cp.erase(pos, 2);
+                if( pos > 0) {
+                    --pos;
+                }
+            }
+            else {
+                ++pos;
+            }
+        }
+        ++loop;
+
+    } while ( cp.size() < marker );
     
-    int pos = 0;
-    while ( copy.size() > 0 ) {
-        if ( copy.substr (pos, 2) == "()" ) {
-            copy.erase(pos, 2);
-        }
-        else {
-            
-        }
+    if ( cp.size() == 0 ) {
+        return 1;
     }
+    else {
+        return 0;
+    }
+    
 }
