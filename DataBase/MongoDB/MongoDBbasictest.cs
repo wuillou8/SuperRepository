9d5159ceb9cb75167574a8bd6dd60f2df22f922d
wuillou8 <wuilloud@gmail.com>
2014-03-30 14:19:14 +0200

    mongodb database handlign in c#

---
+++ DataBase/MongoDB/MongoDBbasictest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+using MongoDB.Driver.Builders;
+using MongoDB.Driver.GridFS;
+using MongoDB.Driver.Linq;
+/*
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver.Wrappers;
+
+using NUnit;
+*/
+
+
+/*
+namespace DATABASE
+{
+    static class MongoDB
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}*/
+
+
+
+namespace DBMONGO//BTCEAPIWRAPPER
+{
+    public class Entity
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+    }
+ 
+    public class Program
+    {
+        public static int Main/*MyDB*/(/*string[] args*/)
+        {
+
+            var connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("test");
+            var collection = database.GetCollection<Entity>("entities");
+
+            var entity = new Entity { Name = "Tom" };
+            collection.Insert(entity);
+            var id = entity.Id;
+
+            var query = Query<Entity>.EQ(e => e.Id, id);
+            entity = collection.FindOne(query);
+
+            entity.Name = "Dick";
+            collection.Save(entity);
+
+            var update = Update<Entity>.Set(e => e.Name, "Harry");
+            collection.Update(query, update);
+            collection.Remove(query);
+            return 2;
+        }
+    }
+}
