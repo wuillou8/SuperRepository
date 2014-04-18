516f1ca72d244ca6f6c7319dd24e5a25dc54eaca
wuillou8 <wuilloud@gmail.com>
2014-04-18 22:54:28 +0200

    adding another bit of ML Julia

--- MLearning/Julia/LinDiscrA/lda.jl
+++ MLearning/Julia/LinDiscrA/lda.jl
@@ -1,36 +1,39 @@
 module LDA
 
+        # Cluster class
 	type MyCluster
-		posx::Float64
-		posy::Float64
+                pos::Array{Float64,1}
 		dim::Int
 		size::Int
 		disp::Array{Float64, }
                 label::Int
-                function MyCluster(posxy::Array{Float64,1},size::Int, dim::Int, label::Int)
-			posx = posxy[1]
-			posy = posxy[2]
+
+                # constructor
+                function MyCluster(posi::Array{Float64,1}, size::Int, dim::Int, label::Int)
+                        # check dim
+                        length(posi) == dim ? pos = posi : throw("error: clust.dim != sizeof(pos_array)")
+                        # generate random points
 			disp = Array(Float64, (size,dim))
 			arr = randn!(disp) 
-			# shift points accordingly
-			arr[1:size] += posx
-			arr[(size+1):end] += posy
-                        new(posx, posy, dim, size, disp, label)
+                        # shift points accordingly
+                        for i in 1:dim
+                                arr[:, i] += pos[i]
+                        end
+                        new(pos, dim, size, disp, label)
 		end
 	end
+
         # function computing the x,y-averages
         function f_mean (m_clus::MyCluster)
 
-                meanx = 0
-                meany = 0
-                for x = m_clus.disp[:, 1]
-                        meanx += x
-                end
-                for y = m_clus.disp[:, 2]
-                        meany += y
-                end
+                f_mean = Array{Float64, (m_clus.dim,1)}
 
-                return meanx/m_clus.size , meany/m_clus.size
+                for d in 1:m_clus.dim
+                        for x = m_clus.disp[:, d]
+                                f_mean[d] += x
+                        end
+                end
+                return f_mean / m_clus.size
         end
 
 end
--- MLearning/Julia/LinDiscrA/main.jl
+++ MLearning/Julia/LinDiscrA/main.jl
@@ -11,16 +11,25 @@ include("lda.jl")
 #########################################################
 
         # construct cluster by hand (point clouds)
-	Dim = 2
+        Dim = 2
         posC1, N1 = [5.5,10.55], 100
         posC2, N2 = [-5.5,2.55], 150
         posC3, N3 = [-0.5,8.55], 150
         posC4, N4 = [2.5,4.55], 150
 
+        # create clusters
         clust1 = LDA.MyCluster(posC1,N1,Dim,1)
         clust2 = LDA.MyCluster(posC2,N2,Dim,2)
         clust3 = LDA.MyCluster(posC3,N3,Dim,3)
         clust4 = LDA.MyCluster(posC4,N4,Dim,4)
+
+        #compute cluster centerd
+        mu1= Array{Float64, 1}
+        LDA.f_mean(clust1))
+        #mu1 = LDA.f_mean(clust1)
+        #mu2_x, mu2_y = LDA.f_mean(clust2)
+        #mu3_x, mu3_y = LDA.f_mean(clust3)
+        #mu4_x, mu4_y = LDA.f_mean(clust4)
 
         # group points within a unique array
         cloud = [ clust1.disp ; clust2.disp ; clust3.disp; clust4.disp ]
@@ -29,8 +38,3 @@ include("lda.jl")
         x, y = cloud[:, 1], cloud[:, 2]
         plot ( x, y, marker="+", linestyle=" ")
         title("Points distribution before running LDA")
-
-        mu1_x, mu1_y = LDA.f_mean(clust1)
-        mu2_x, mu2_y = LDA.f_mean(clust2)
-        mu3_x, mu3_y = LDA.f_mean(clust3)
-        mu4_x, mu4_y = LDA.f_mean(clust4)
