d9b654c7e9704b5c065994572fc93ed4c9b61776
wuillou8 <wuilloud@gmail.com>
2015-09-04 16:16:23 +0100

    correct poisson law

--- Simulations/Demo_Data_Driven_Clojure/multi_armed_bandit/project.clj
+++ Simulations/Demo_Data_Driven_Clojure/multi_armed_bandit/project.clj
@@ -4,7 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojure "1.6.0"]
                  [incanter "1.5.6"]])
-                 ;[incanter/incanter-core "1.5.6"]
-                 ;[incanter/incanter-charts "1.5.6"]])
