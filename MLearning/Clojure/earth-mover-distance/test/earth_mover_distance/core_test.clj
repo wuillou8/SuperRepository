e6b0546c14de2561d29e3c87d581ae8dd68ba3cf
wuillou8 <wuilloud@gmail.com>
2015-09-25 22:18:24 +0100

    russel algo in clojure

--- MLearning/Clojure/earth-mover-distance/test/earth_mover_distance/core_test.clj
+++ MLearning/Clojure/earth-mover-distance/test/earth_mover_distance/core_test.clj
@@ -7,10 +7,10 @@
 ; E.j. Russel, Extension of Dantzig's Algorithm to finding an initial
 ; near-optimal basis for the transportation problem , operations research 1969 
 
-(defrecord transportation-pbm [m-costs v-supply v-demand])
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; example from Russel paper, Houthaker matrix
+
 (def houthakker-matrix 
   (array 
     [[73 40 9 79 20]
@@ -27,6 +27,48 @@
 (deftest test-phantomjs-simulation
  (is (= 1104 (run-russel traspo)))
  )
+
+; example 1
+
+(defrecord feature [x y z])
+(def f-1 [(->feature 100 40 22) (->feature 211 20 2) (->feature 32 190 150) (->feature 2 100 100)])
+(def f-2 [(->feature 0 0 0) (->feature 50 100 80)  (->feature 255 255 255)])
+
+;weights
+
+(def w-1  [0.4 0.3 0.2 0.1]) ; supply
+(def w-2  [0.5 0.3 0.2]) ; demand
+
+(defn distance-fct [f1, f2]
+  (sqrt
+    (+ (pow (- (:x f1) (:x f2)) 2)
+       (pow (- (:y f1) (:y f2)) 2)
+       (pow (- (:z f1) (:z f2)) 2))))
+       
+(def c (array
+  (for [el f-1] 
+    (mapv #(distance-fct el %)  f-2)))) ;)  
+
+(def transpo1 (->transportation-pbm c w-1 w-2))
+;(println "cost: " (run-russel transpo1))
+
+
+
+(deftest test-russel-article 
+ (is (= 1104 (run-russel traspo))))
+
+(deftest test-example1
+  (is (= 163.07130724471503 (run-russel transpo1))))
+
+
+
+
+
+
+
+
+
+
 
 
 ;(loop [transport-pbm traspo cost 0]
